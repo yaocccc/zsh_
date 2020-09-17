0920ffe18dd909a7abe30c935a202c7121f4db26
chenyc <flyforest@outlook.com>
2020-09-17 12:01:49 +0800

    zsh

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 init_linux() {
     touch ~/.zshrc
+    mkdir -p ~/.config/zsh/cache
     if [ `grep -c "source ~/.config/zsh/init.zshrc" ~/.zshrc` -eq '0' ]; then
         echo "source ~/.config/zsh/init.zshrc" >> ~/.zshrc
         source ~/.zshrc
--- script/backup/backup.sh
+++ script/backup/backup.sh
@@ -2,11 +2,13 @@
 
 backup() {
     mkdir -p ~/backups/linux
-    sudo rsync -Pa / ~/backups/linux --exclude=/media/* --exclude=/sys/* --exclude=/proc/* --exclude=/mnt/* --exclude=/tmp/* --exclude=~/backups/linux/*
+    sudo rsync -Pa / ~/backups/linux --exclude=/media/* --exclude=/sys/* --exclude=/proc/* --exclude=/mnt/* --exclude=/tmp/* --exclude=/home/chenyc/backups/linux/*
+    clear && echo -e '\033[43;30mbackup completed :) \033[0m'
 }
 
 recovery() {
     sudo rsync -Pa ~/backups/linux/ /
+    clear && echo -e '\033[43;30mrecovery completed:) \033[0m'
 }
 
 case $1 in
@@ -17,7 +19,3 @@ case $1 in
         recovery
         ;;
 esac
-
-clear && echo -e '\033[43;30minited :) \033[0m'
-
-
