7120e1e8225d8b6a29d1b36168282243a51b004a
chenyc <flyforest@outlook.com>
2020-09-18 13:13:36 +0800

    zsh

--- script/backup/backup.sh
+++ script/backup/backup.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
 
 backup() {
-    mkdir -p ~/backups/linux
-    sudo rsync -Pa / ~/backups/linux --exclude=/media/* --exclude=/sys/* --exclude=/proc/* --exclude=/mnt/* --exclude=/tmp/* --exclude=*/node_modules/* --exclude=/home/chenyc/backups/linux/*
-    clear && echo -e '\033[43;30mbackup completed :) \033[0m'
+    local from_dir=/
+    local to_dir=/home/chenyc/backups/linux
+    mkdir -p $to_dir
+    sudo rsync -Pa $from_dir $to_dir --exclude=/media/* --exclude=/sys/* --exclude=/proc/* --exclude=/mnt/* --exclude=/tmp/* --exclude=*/node_modules/* --exclude=/home/chenyc/backups/linux/*
+    echo -e '\033[43;30mbackup completed :) \033[0m'
 }
 
 recovery() {
-    sudo rsync -Pa ~/backups/linux/ /
-    clear && echo -e '\033[43;30mrecovery completed:) \033[0m'
+    local from_dir=/home/chenyc/backups/linux/
+    local to_dir=/
+    sudo rsync -Pa $from_dir $to_dir
+    echo -e '\033[43;30mrecovery completed:) \033[0m'
 }
 
 case $1 in
