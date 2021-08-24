3e942bb59741b8f98125ac4fa3d730c81b75a88e
chenyc <flyforest@outlook.com>
2021-08-24 14:38:07 +0800

    update

--- config/git.zsh
+++ config/git.zsh
@@ -2,14 +2,23 @@ alias git='git --no-pager'
 alias gco='git checkout'
 alias ginit='git init && echo "[user]\n    email = flyforest@outlook.com" >> .git/config && cp ~/.gitignore .'
 alias gpo='git push origin $(git symbolic-ref --short -q HEAD)'
-alias gpt='git push origin --tags'
 alias gpl='git pull origin $(git symbolic-ref --short -q HEAD) --ff-only'
 alias gd='git diff'
 alias gs='git status'
 alias gss='git status -s'
+alias gpt='git push origin --tags'
+alias glt='git tag -n --sort=taggerdate | tail -n ${1-10}'
+gat() { git tag -a $1 -m "$2" }
 gam() { git add --all && git commit -m "$*" }
 gitlog() {
-    git --no-pager log --pretty=format:$1 --graph   -n ${2-10} \
+    git --no-pager log --pretty=format:$1 --graph -n ${2-10} \
 }
 gll() { gitlog "%h %s" $1 }
 glll() { gitlog "%H %cd %cn: %s" $1 }
+
+# git tag
+# git show V1.2
+# git push origin --tags
+# git tag -d V1.2
+# git push origin :refs/tags/V1.2
+# git fetch origin tag V1.2
