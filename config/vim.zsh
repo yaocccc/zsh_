498ac1af6bc10f30c8407fba503bd98c5653b821
chenyc <flyforest@outlook.com>
2021-09-14 10:15:39 +0800

    update

--- config/vim.zsh
+++ config/vim.zsh
@@ -1,4 +1,4 @@
 vim() { if [[ $* && -d $* ]] { cd $* && nvim } else { nvim $* } }
 alias T='dir=~/backups/todo && mkdir -p $dir && vim $dir/todo.md'
 alias vzc='vim $ZSH/init.zshrc'
-alias vrc='cd ~/.config/nvim && vim init.vim'
+alias vrc='vim ~/.config/nvim/init.vim'
