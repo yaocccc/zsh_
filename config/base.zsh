1df3954c48a0259249b2834e82ab49fe22bceccb
chenyc <flyforest@outlook.com>
2022-04-10 15:01:28 +0800

    update

--- config/base.zsh
+++ config/base.zsh
@@ -4,32 +4,23 @@ export TRASH=~/.local/share/Trash/files
 
 alias S='startx'
 alias dx='sudo du -h -d 1'
-alias getip="ip addr show | grep '192.168.' | awk '{print \$2}'"
 alias sc='~/scripts/set-screen.sh'
 alias scc='~/scripts/set-wallpaper.sh'
 alias bl='~/scripts/bluetooth.sh'
 alias gif='~/scripts/gif-recorder.sh'
 alias vpn='~/scripts/app-starter.sh vpn '
 alias vpn2='~/scripts/app-starter.sh vpn2 '
-alias google='web_search google'
-alias baidu='web_search baidu'
-alias C='~/scripts/app-starter.sh clock && exit'
 alias surf='~/scripts/app-starter.sh surf'
-alias ast='~/scripts/app-starter.sh ast'
 alias ssh='~/.ssh/ssh.sh'
+alias scp='~/.ssh/scp.sh'
 alias rl='sudo systemctl restart NetworkManager.service'
 alias up='st -e nvim +PlugUpdate\|CocUpdate >> /dev/null 2>&1 & sudo pacman -Syyu'
-alias music='~/scripts/app-starter.sh music'
-alias weather='_weather >/dev/null 2>&1'
 
-_weather() {
-    notify-send "$(date '+%Y-%m-%d')" "$(curl 'wttr.in/ShangHai?format=3')\n$(curl 'wttr.in/WenZhou?format=3')"
-}
 tp() {
     [ "$(docker ps | grep v2raya)" ] && docker stop v2raya || docker restart v2raya
     ~/scripts/dwm-status.sh
 }
-c() {
+col() {
     awk '{print $'$1'}'
 }
 rm() {
@@ -44,10 +35,10 @@ rm() {
 }
 docker() {
     case $* in
-        restart) sudo docker restart $(sudo docker ps -a | sed 1d | c 1) ;;
-        stop) sudo docker stop $(sudo docker ps -a | sed 1d | c 1) ;;
-        rm) sudo docker rm $(sudo docker ps -a | sed 1d | c 1) ;;
-        rmi) sudo docker rmi $(sudo docker images | sed 1d | c 3) ;;
+        restart) sudo docker restart $(sudo docker ps -a | sed 1d | col 1) ;;
+        stop) sudo docker stop $(sudo docker ps -a | sed 1d | col 1) ;;
+        rm) sudo docker rm $(sudo docker ps -a | sed 1d | col 1) ;;
+        rmi) sudo docker rmi $(sudo docker images | sed 1d | col 3) ;;
         ps) sudo docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Status}}" ;;
         *) sudo docker $* ;;
     esac
--- config/vim.zsh
+++ config/vim.zsh
@@ -1,4 +1,4 @@
 vim() { if [[ $* && -d $* ]] { cd $* && nvim } else { nvim $* } }
-alias T='dir=~/backups/todo && mkdir -p $dir && vim $dir/todo.md'
+alias T='dir=~/backups/todo && mkdir -p $dir && vim -O $dir/todo.md $dir/note.md'
 alias vzc='vim $ZSH/init.zshrc'
 alias vrc='vim ~/.config/nvim/init.vim'
